793e3ff0e5bf57fa934dcfc2e1de5a9523250aab
Brian Westerweel <deprehend0@gmail.com>
2025-01-31 14:13:47 +0100

    Feature: added prints for when the opCode is invalid. Feature: added a check to prevent division by zero exceptions. Chore: implemented block code for the first time in this course

--- CalcEngine/src/deprehend0/calcengine/Main.java
+++ CalcEngine/src/deprehend0/calcengine/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
   public static void main(String[] args) {
     double value1 = 100d;
-    double value2 = 50d;
-    double result = 0;
+    double value2 = 0d;
+    double result = 0d;
     char opCode = 'd'; // Operation code (a: add, s: subtract, m: multiply, d: divide)
 
     if (opCode == 'a')
@@ -14,10 +14,14 @@ public class Main {
       result = value1 - value2;
     else if (opCode == 'm')
       result = value1 * value2;
-    else if (opCode == 'd')
-      result = value1 / value2;
-    else
+    else if (opCode == 'd') {
+      if (value2 != 0)
+        result = value1 / value2;
+    }
+    else {
+      System.out.println("Invalid opCode: " + opCode);
       result = 0.0d;
+    }
 
     System.out.println(result);
   }
