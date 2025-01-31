7e9e3f7ea7ac61603d390e38fb8e623e95fd4ab8
Brian Westerweel <deprehend0@gmail.com>
2025-01-31 16:00:17 +0100

    Feature: added commandline argument support

--- CalcEngine/src/deprehend0/calcengine/Main.java
+++ CalcEngine/src/deprehend0/calcengine/Main.java
@@ -8,13 +8,28 @@ public class Main {
     char[] opCodes = {'d', 'a', 's', 'm'};
     double[] results = new double[opCodes.length];
 
-    for(int i = 0; i < opCodes.length; i ++) {
-      results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
-    }
+    if (args.length == 0) {
+      for (int i = 0; i < opCodes.length; i++) {
+        results[i] = execute(opCodes[i], leftVals[i], rightVals[i]);
+      }
 
-    for (double result : results) {
-      System.out.println(result);
+      for (double result : results) {
+        System.out.println(result);
+      }
+    } else if (args.length == 3) {
+      handleCommandLine(args);
+    } else {
+      System.out.println("Please provide an operation code (a, d, s, m) and 2 numeric values");
     }
+  }
+
+  static void handleCommandLine(String[] args) {
+    char opCode = args[0].charAt(0);
+    double leftVal = Double.parseDouble(args[1]);
+    double rightVal = Double.parseDouble(args[2]);
+
+    double result = execute(opCode, leftVal, rightVal);
+    System.out.println(result);
   }
 
   static double execute(char opCode, double leftVal, double rightVal) {
