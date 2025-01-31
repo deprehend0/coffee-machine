5510dc3d55233fdc7391da7dca0fbbc46252c081
Brian Westerweel <deprehend0@gmail.com>
2025-01-31 14:31:37 +0100

    Chore: Transformed the if-statements to a switch case

--- CalcEngine/src/deprehend0/calcengine/Main.java
+++ CalcEngine/src/deprehend0/calcengine/Main.java
@@ -4,23 +4,27 @@ public class Main {
 
   public static void main(String[] args) {
     double value1 = 100d;
-    double value2 = 0d;
+    double value2 = 2d;
     double result = 0d;
     char opCode = 'd'; // Operation code (a: add, s: subtract, m: multiply, d: divide)
 
-    if (opCode == 'a')
-      result = value1 + value2;
-    else if (opCode == 's')
-      result = value1 - value2;
-    else if (opCode == 'm')
-      result = value1 * value2;
-    else if (opCode == 'd') {
-      if (value2 != 0)
-        result = value1 / value2;
-    }
-    else {
-      System.out.println("Invalid opCode: " + opCode);
-      result = 0.0d;
+    switch(opCode){
+      case 'a':
+        result = value1 + value2;
+        break;
+      case 's':
+        result = value1 - value2;
+        break;
+      case 'm':
+        result = value1 * value2;
+        break;
+      case 'd':
+        if (value2 != 0)
+          result = value1 / value2;
+        break;
+      default:
+        System.out.println("Invalid opCode: " + opCode);
+        result = 0.0d;
     }
 
     System.out.println(result);
