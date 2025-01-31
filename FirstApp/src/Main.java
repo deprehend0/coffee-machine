df8ab86c0ac0b7b4f7301ba95a97fca4f4c8312d
Brian Westerweel <bwesterweel@Air-van-Brian.home>
2025-01-31 09:28:39 +0100

    Followed the steps for the FirstApp Example of Jim Wilson's Java SE Fundamentals path in PluralSight

---
+++ FirstApp/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    /*
+        First comment line
+        Second comment line
+        // Nested line comment
+     */
+    public static void main(String[] args) {
+        // This is a standalone line comment
+        System.out.println("Hello, World!"); // This is a line comment
+//        System.out.println("Second line :)");
+        System.out.println(/*"Goodbye my lover <3"*/ "This is just a tribute");
+    }
+}
