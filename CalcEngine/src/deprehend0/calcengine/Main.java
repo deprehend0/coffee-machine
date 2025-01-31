4e9e68ce120c5af66cbf9e5e4c6f56e843e6c1c8
Brian Westerweel <deprehend0@gmail.com>
2025-01-31 12:24:26 +0100

    Feature: Created some basic (ugly) code for following the basic logical relational operators and doing some basic math

--- CalcEngine/src/deprehend0/calcengine/Main.java
+++ CalcEngine/src/deprehend0/calcengine/Main.java
@@ -3,8 +3,22 @@ package deprehend0.calcengine;
 public class Main {
 
   public static void main(String[] args) {
+    double value1 = 100d;
+    double value2 = 50d;
+    double result = 0;
+    char opCode = 'd'; // Operation code (a: add, s: subtract, m: multiply, d: divide)
 
+    if (opCode == 'a')
+      result = value1 + value2;
+    else if (opCode == 's')
+      result = value1 - value2;
+    else if (opCode == 'm')
+      result = value1 * value2;
+    else if (opCode == 'd')
+      result = value1 / value2;
+    else
+      result = 0.0d;
 
-
+    System.out.println(result);
   }
 }
